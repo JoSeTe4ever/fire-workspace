90368b88ea9d02b40c5a3ab97e3d2b27a125f12c
jmpo <joseh.parreno@gmail.com>
2019-10-21 16:57:00 +0200

    initial commit

---
+++ apps/spark/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'spark',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/spark'
+};
